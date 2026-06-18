0f5bb044ac27765cbe338c0b9b29cd159287b329
David Uhden Collado <david@uhden.dev>
2026-06-18 20:36:20 +0200

    Try to use the cabal module

---
+++ simplexmq/files/UnixTimeConfig.h
@@ -0,0 +1,18 @@
+#ifndef UNIX_TIME_CONFIG_H
+#define UNIX_TIME_CONFIG_H
+
+#define HAVE_INTTYPES_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_TIMEGM 1
+#define HAVE_UNISTD_H 1
+#define IS_LINUX 0
+#define IS_NT61 0
+#define STDC_HEADERS 1
+
+#endif

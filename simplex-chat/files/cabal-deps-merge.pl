31b81161c63f4fa172ac91fb38bc41d4745a54df
David Uhden Collado <david@uhden.dev>
2026-06-26 22:53:37 +0200

    Update simplex ports

--- simplex-chat/files/cabal-deps-merge.pl
+++ simplex-chat/files/cabal-deps-merge.pl
@@ -14,34 +14,65 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #
-# Merge cabal-bundler output from multiple executables into a single
-# MODCABAL_MANIFEST, skipping vendored packages and injecting extras.
+# Merge cabal-bundler output from multiple executables and/or cabal
+# plan.json files into a single MODCABAL_MANIFEST, skipping vendored
+# packages and injecting extras.
 #
 # Environment:
 #   SKIP   comma-separated package names to exclude (e.g. aeson,socks)
 #   EXTRA  comma-separated name:version:revision triplets to inject
 #
 # Usage:
-#   env SKIP=... EXTRA=... perl cabal-deps-merge.pl inc1 inc2 > cabal.inc
+#   env SKIP=... EXTRA=... perl cabal-deps-merge.pl inc1 inc2 plan.json > cabal.inc
 
 use strict;
 use warnings;
+use JSON::PP;
 
 my %skip = map { $_ => 1 } split /,/, $ENV{SKIP} // '';
 my %seen;
+my %seen_nv;
 
 for my $dep ( split /,/, $ENV{EXTRA} // '' ) {
     next unless length $dep;
     my ( $n, $v, $r ) = split /:/, $dep;
     next                    if $skip{$n};
-    $seen{"$n\t$v\t$r"} = 1 if defined $r;
+    if ( defined $r ) {
+        $seen{"$n\t$v\t$r"} = 1;
+        $seen_nv{"$n\t$v"} = 1;
+    }
 }
 
-while (<>) {
-    next unless /^\s+(\S+)\s+(\S+)\s+(\d+)\s*\\?$/;
-    my ( $n, $v, $r ) = ( $1, $2, $3 );
-    next if $skip{$n};
-    $seen{"$n\t$v\t$r"} = 1;
+for my $file (@ARGV) {
+    if ( $file =~ /(?:^|\/|\.)(?:test\.)?plan\.json$/ ) {
+        my $json = do {
+            open my $fh, '<', $file or die "$file: $!";
+            local $/;
+            <$fh>;
+        };
+        my $plan = decode_json($json);
+        for my $unit ( @{ $plan->{'install-plan'} // [] } ) {
+            my $src = $unit->{'pkg-src'} // next;
+            next unless ref $src eq 'HASH' && ( $src->{type} // '' ) eq 'repo-tar';
+            my $n = $unit->{'pkg-name'} // next;
+            my $v = $unit->{'pkg-version'} // next;
+            my ($r) = ( $src->{'cabal-file-url'} // '' ) =~ m{/revision/(\d+)};
+            $r //= 0;
+            next if $skip{$n};
+            next if $seen_nv{"$n\t$v"};
+            $seen{"$n\t$v\t$r"} = 1;
+            $seen_nv{"$n\t$v"} = 1;
+        }
+        next;
+    }
+    open my $fh, '<', $file or die "$file: $!";
+    while (<$fh>) {
+        next unless /^\s+(\S+)\s+(\S+)\s+(\d+)\s*\\?$/;
+        my ( $n, $v, $r ) = ( $1, $2, $3 );
+        next if $skip{$n};
+        $seen{"$n\t$v\t$r"} = 1;
+        $seen_nv{"$n\t$v"} = 1;
+    }
 }
 
 my @deps = sort keys %seen;
